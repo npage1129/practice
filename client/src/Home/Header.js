a05664da6cbe24ce7c305e8eadafc32e0a939c51
Nicole Page <npage1129@gmail.com>
2022-09-15 19:28:41 -0400

    Header

---
+++ client/src/Home/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Header() {
+return(
+    <div>Hello!</div>
+)
+}
+export default Header;
--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Header';
 
 const Home =()=>{
 return (
     <div>
-     hello!   
+     <Header/>   
     </div>
+    
 )
 }
 export default Home;
