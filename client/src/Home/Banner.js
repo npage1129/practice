b32ed483229d0fdf8b8571acc2b8e47bf4b3d538
Nicole Page <npage1129@gmail.com>
2022-10-03 09:27:51 -0600

    banner done

--- client/src/Home/Banner.js
+++ client/src/Home/Banner.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import {Container} from 'react-bootstrap'
 import './Banner.css'
 
 function Banner(){
     return(
         <div className = "cards">
-<div class="container text-center">
-  <div class="row">
-    <div class="col">
-      <img id = "record" src= "https://www.kindpng.com/picc/m/46-468121_vinyl-record-png-circle-transparent-png.png"/>
-    </div>
-    <div class="col">
-      <img id = "guitar" src= "https://www.pngitem.com/pimgs/m/47-476223_guitarist-people-playing-instruments-png-transparent-png.png" />
-    </div>
-  
-  </div>
-</div>
+
+      <img id = "male" src= "https://www.nicepng.com/png/detail/218-2188610_free-png-playing-bass-png-images-transparent-people.png"/>
+
+      <img id = "female" src= "https://www.pngitem.com/pimgs/m/47-476223_guitarist-people-playing-instruments-png-transparent-png.png" />
+
+
     </div>
     )
 }
