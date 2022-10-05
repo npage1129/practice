221edbf1d3bd53e5f28ae7f6b5fbaa148d922b31
Nicole Page <npage1129@gmail.com>
2022-10-04 18:13:46 -0600

    grid

---
+++ client/src/Home/Guitar_Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Guitar_Card';
+
+function Guitar_Card(){
+    return(
+        <div class="container text-center">
+    <div class="row row-cols-4">
+    <div class="col-sm">Column</div>
+    <div class="col-sm">Column</div>
+    <div class="col-sm">Column</div>
+    <div class="col-sm">Column</div>
+    </div>
+</div>
+    )
+}
+
+export default Guitar_Card;
--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Banner from './Banner';
+import Guitar_Card from './Guitar_Card';
+
 const Home =()=>{
 return (
     <div>
      <Header/> 
      <Banner/>  
+    <Guitar_Card/>
     </div>
     
 )
